bded5a01039991ab5b087a21e8c5c2009b1d883c
owenholic <couragepack@gmail.com>
2022-04-12 16:56:18 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,6 @@
  *
  * Return: Always 0.
  */
-
  void print_alphabet(void)
 {
 char alphabet;
@@ -15,5 +14,6 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 _putchar(alphabet);
 }
 _putchar('\n');
+
 return (0);
 }

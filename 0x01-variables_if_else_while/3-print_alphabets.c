23d7cfe077dd0cb0998e8a874a113041ea76c4a0
owenholic <couragepack@gmail.com>
2022-04-12 04:21:05 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,7 +5,6 @@
  * Return; Always 0 (sucess/correct)
  */
 int main(void)
-
 {
 char lower_case;
 char upper_case;
@@ -20,8 +19,6 @@ for (upper_case = 'A'; upper_case <= 'Z' ; upper_case++)
 putchar(upper_case);
 }
 
-putchar('\n);
-
-return (0) ;
-
+putchar('\n');
+return (0);
 }

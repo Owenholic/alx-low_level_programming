4f19448419387a2996cb254249c5b10e6374b4ca
owenholic <couragepack@gmail.com>
2022-04-12 02:25:04 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+
+{
+char base_16;
+
+for (base_16 = 48; base_16 <= 57; base_16++)
+{putchar(base_16);
+}
+putchar ('\n');
+return (0);
+}

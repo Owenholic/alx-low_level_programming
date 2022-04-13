1269f4acded1d9a69b8caffc42c9ecac8521c6e2
owenholic <couragepack@gmail.com>
2022-04-12 17:27:35 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-include "main.h"
+#include "main.h"
 
 /**
  * _islower - short description, single line
  * @c: contains value to be compared
  * Return: Always 0.
- *
  */
 
 int _islower(int c)

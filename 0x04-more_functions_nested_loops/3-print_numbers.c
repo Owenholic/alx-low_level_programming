5bc714052a3d171aa9b90f2c625b32a9236c5981
owenholic <couragepack@gmail.com>
2022-04-15 15:28:48 -0700

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_numbers - print numbers
@@ -11,7 +12,7 @@ i=0;
 while (i < 10)
 {
 _putchar (i = '0');
-i++'
+i++;
 }
 _putchar ('/n');
 return (0);

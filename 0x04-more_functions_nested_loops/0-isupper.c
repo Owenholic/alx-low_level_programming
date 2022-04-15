adbb872e3bc7494cb2512dc3817f5f0e354164b3
owenholic <couragepack@gmail.com>
2022-04-15 14:42:35 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _isupper - evalue if letter is uppercase.
- *@c: print int
+ * _isupper - evalue if letter is uppercase .
+ * @c: print int
  * Return: Always 0.
  */
 
@@ -15,5 +15,4 @@ else
 {
 return (0);
 }
-
 }

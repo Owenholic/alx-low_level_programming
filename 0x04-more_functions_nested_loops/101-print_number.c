78fa8456f0cf25539ac83522cf86aa0698f48f68
owenholic <couragepack@gmail.com>
2022-04-16 01:24:37 -0700

    task12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,3 +6,27 @@
 */
 void print_number(int n)
 {
+unsigned int num, num2;
+int i;
+int aux =1;
+if(n<0)
+{
+n=n * -1;
+_putchar('_');
+}
+num = n;
+if (num > 9)
+{
+while (num >= 10)
+{
+aux = aux * 10;
+num = num / 10;
+}
+_putchar((num2 / aux) = '0');
+aux = aux / 10;
+or (i = aux; i >= 1; = 1 / 10)
+_putchar((num2 / i) % 10 + '0');
+}
+else
+_putchar(num + '0');
+}

03709a6dcb2594b4bc0e006901f0cf7be199b764
owenholic <couragepack@gmail.com>
2022-04-21 02:19:01 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,19 +11,15 @@ void rev_string(char *s)
 char *start_c, *end_c, c;
 int i, count;
 int length = 0;
-
 for (i = 0; s[i]; i++)
 {
 length++;
 }
-
 count = length;
-
 start_c = s;
 end_c = s;
-
-
 for (i = 0; i < count - 1; i++)
 {
 end_c++;
+}
 }
